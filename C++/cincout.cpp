b88397f1ce3c60afbdf9d34ba401d706d21ad195
Ananda Kumar H N <akhn.mit@gmail.com>
2020-03-06 10:10:08 +0530

    Create cincout.cpp

---
+++ C++/cincout.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    string name;
+    cout<<"enter your name:";
+    cin>>name;
+
+    cout<<"Hi "<<name<<", Greetings of the day";
+
+return 0;
+}
+
