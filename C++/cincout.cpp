c8bd121c24295687644bab3b4730e99fa22d4da7
Ananda Kumar H N <akhn.mit@gmail.com>
2020-03-10 11:30:56 +0530

    Update cincout.cpp

--- C++/cincout.cpp
+++ C++/cincout.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 int main()
 {
-
+    char a[100];
     string name;
     cout<<"enter your name:";
-    cin>>name;
-
+//    cin>>name;
     cout<<"Hi "<<name<<", Greetings of the day";
+
+    int p=20;
+    int &x=p;
+    cout<<endl<<p<<endl<<x;
+    x++;
+    cout<<endl<<p<<endl<<x;
 
 return 0;
 }
