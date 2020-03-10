53f3e9bc489fa885b8f27fe004f4933c9178d3ac
Ananda Kumar H N <akhn.mit@gmail.com>
2020-03-10 11:20:02 +0530

    Create inline.cpp

---
+++ C++/inline.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+inline int add(int x, int y)
+{
+    return (x+y);
+}
+inline float add(float x, float y)
+{
+    return (x+y);
+}
+
+
+int main()
+{
+    cout<<"function overloading"<<endl;
+
+    int p=120,q=23;
+    float s=38.99,r=45.467;
+
+
+    cout<<endl<<p<<endl<<q<<endl<<r<<endl<<s<<endl;
+
+    cout<<"sum="<<add(p,q)<<endl;
+    cout<<"sum="<<add(s,r)<<endl;
+
+
+
+return 0;
+}
+
